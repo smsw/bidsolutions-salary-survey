96c6410204e563437a0f816b53f06ee229008dbb
Grant Bartlett <grantbartlett@Grants-MacBook-Pro.local>
2015-05-30 21:40:56 +0100

    Changing tabs to spaces indent

--- public/modules/salaries/controllers/salaries.client.controller.js
+++ public/modules/salaries/controllers/salaries.client.controller.js
@@ -2,8 +2,8 @@
 
 // Salaries controller
 angular.module('salaries').controller('SalariesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Salaries',
-	function($scope, $stateParams, $location, Authentication, Salaries) {
-		$scope.authentication = Authentication;
+    function($scope, $stateParams, $location, Authentication, Salaries) {
+        $scope.authentication = Authentication;
 
         // Calculate Average Salary
         $scope.calculateAvg = function(){
@@ -34,11 +34,11 @@ angular.module('salaries').controller('SalariesController', ['$scope', '$statePa
             loading: false
         };
 
-		// Create new Salary
-		$scope.create = function() {
-			// Create new Salary object
-			var salary = new Salaries ({
-				name: this.name,
+        // Create new Salary
+        $scope.create = function() {
+            // Create new Salary object
+            var salary = new Salaries ({
+                name: this.name,
                 salary: this.salary,
                 bonus: this.bonus,
                 gender: this.gender,
@@ -47,57 +47,57 @@ angular.module('salaries').controller('SalariesController', ['$scope', '$statePa
                 working_practice: this.working_practice
             });
 
-			// Redirect after save
-			salary.$save(function(response) {
-				$location.path('salaries/' + response._id);
+            // Redirect after save
+            salary.$save(function(response) {
+                $location.path('salaries/' + response._id);
 
-				// Clear form fields
-				$scope.name = '';
+                // Clear form fields
+                $scope.name = '';
                 $scope.salary = '';
-				$scope.bonus = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+                $scope.bonus = '';
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Remove existing Salary
-		$scope.remove = function(salary) {
-			if ( salary ) { 
-				salary.$remove();
+        // Remove existing Salary
+        $scope.remove = function(salary) {
+            if ( salary ) {
+                salary.$remove();
 
-				for (var i in $scope.salaries) {
-					if ($scope.salaries [i] === salary) {
-						$scope.salaries.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.salary.$remove(function() {
-					$location.path('salaries');
-				});
-			}
-		};
+                for (var i in $scope.salaries) {
+                    if ($scope.salaries [i] === salary) {
+                        $scope.salaries.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.salary.$remove(function() {
+                    $location.path('salaries');
+                });
+            }
+        };
 
-		// Update existing Salary
-		$scope.update = function() {
-			var salary = $scope.salary;
+        // Update existing Salary
+        $scope.update = function() {
+            var salary = $scope.salary;
 
-			salary.$update(function() {
-				$location.path('salaries/' + salary._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+            salary.$update(function() {
+                $location.path('salaries/' + salary._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Find a list of Salaries
-		$scope.find = function() {
-			$scope.salaries = Salaries.query();
-		};
+        // Find a list of Salaries
+        $scope.find = function() {
+            $scope.salaries = Salaries.query();
+        };
 
-		// Find existing Salary
-		$scope.findOne = function() {
-			$scope.salary = Salaries.get({ 
-				salaryId: $stateParams.salaryId
-			});
-		};
-	}
+        // Find existing Salary
+        $scope.findOne = function() {
+            $scope.salary = Salaries.get({
+                salaryId: $stateParams.salaryId
+            });
+        };
+    }
 ]);
