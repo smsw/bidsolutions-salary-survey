64f2c4dc97e0aec2c7ac503a094608ffee0db983
GrantBartlett <hello@grant-bartlett.com>
2015-06-12 16:47:57 +0100

    Moving salary filters logic out of controller into a new

--- public/modules/salaries/controllers/salaries.client.controller.js
+++ public/modules/salaries/controllers/salaries.client.controller.js
@@ -5,8 +5,6 @@ angular.module('salaries').controller('SalariesController', ['$scope', '$statePa
     function ($scope, $stateParams, $location, $filter, Authentication, Salaries) {
         $scope.authentication = Authentication;
 
-        var Salary = Salaries.query();
-
         // Create new Salary
         $scope.create = function () {
             // Create new Salary object
@@ -86,79 +84,6 @@ angular.module('salaries').controller('SalariesController', ['$scope', '$statePa
                 salaryId: $stateParams.salaryId
             });
         };
-
-        // Called on the view with ng-init
-        // When Data is ready, run functions that require our promised data
-        $scope.initSalaryFunctions = function () {
-            Salary.$promise.then(function (data) {
-
-                // Check All data is here
-                console.log('All data', data);
-
-                // Sort data by job title
-                console.log('By job title', $scope.filterData('Document Manager','bs_job_title', data));
-
-                // Example of putting Document manager results into DataColumn[]
-                $scope.populateDataColumn($scope.filterData('Document Manager','bs_job_title', data));
-            });
-        };
-
-        /***
-         * Filter data by various params
-         * @param filter
-         * @param prop
-         * @param data
-         * @returns {Array}
-         */
-        $scope.filterData = function (filter, prop, data) {
-            var result = [];
-
-            angular.forEach(data, function (value, key) {
-                if (value[prop] === filter) {
-                    result.push(value);
-                }
-            });
-            return result;
-        };
-
-
-        /***
-         * Populate DataColumn Model with our results
-         * @param data
-         * @returns {Number}
-         */
-        $scope.populateDataColumn = function (data) {
-            $scope.dataColumns = [];
-            return $scope.dataColumns.push
-            (
-                {
-                    name: 'Document Manager',
-                    salary: {
-                        minimum: null,
-                        average: null,
-                        median: null,
-                        maximum: null
-                    },
-                    average_age: null,
-                    raw: data
-                }
-            );
-        };
-
-
-        // Example model for our Data columns we require
-        $scope.exampleDataColumModel = [
-            {
-                "name": "Document Manager",
-                "salary": {
-                    minimum: 100,
-                    average: 200,
-                    median: 300,
-                    maximum: 400
-                },
-                "average_age": 35
-            }
-        ];
     }
 ]);
 
