181a46c034a2115b049ddca124eb99869743d153
Grant Bartlett <grant@smswmedia.com>
2015-06-24 10:19:41 +0100

    Removing salary route for now

--- app/routes/salaries.server.routes.js
+++ app/routes/salaries.server.routes.js
@@ -1,23 +1,23 @@
 'use strict';
 
 module.exports = function (app) {
-    var users = require('../../app/controllers/users.server.controller');
-    var salaries = require('../../app/controllers/salaries.server.controller');
+    //var users = require('../../app/controllers/users.server.controller');
+    //var salaries = require('../../app/controllers/salaries.server.controller');
     var filteredSalaries = require('../../app/controllers/salaries-filter.server.controller');
 
     // Salaries Routes
-    app.route('/salaries')
-        .get(salaries.list)
-        .post(users.requiresLogin, salaries.create);
+    //app.route('/salaries')
+    //    .get(salaries.list)
+    //    .post(users.requiresLogin, salaries.create);
 
     app.route('/salaries/search')
         .get(filteredSalaries.list);
 
-    app.route('/salaries/:salaryId')
-        .get(salaries.read)
-        .put(users.requiresLogin, salaries.hasAuthorization, salaries.update)
-        .delete(users.requiresLogin, salaries.hasAuthorization, salaries.delete);
-
-    // Finish by binding the Salary middleware
-    app.param('salaryId', salaries.salaryByID);
+    //app.route('/salaries/:salaryId')
+    //    .get(salaries.read)
+    //    .put(users.requiresLogin, salaries.hasAuthorization, salaries.update)
+    //    .delete(users.requiresLogin, salaries.hasAuthorization, salaries.delete);
+    //
+    //// Finish by binding the Salary middleware
+    //app.param('salaryId', salaries.salaryByID);
 };
